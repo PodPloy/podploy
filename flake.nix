081f0388f01e049f7ba136e1e16e45f20fb56f86
DanteDev2102 <jdap.dmc@gmail.com>
2026-01-30 23:14:09 -0400

    chore: flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Go Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          go-task
+          air
+          mockery
+          golangci-lint
+          atlas
+          protobuf
+          protoc-gen-go
+          protoc-gen-go-grpc
+        ];
+
+        shellHook = ''
+          export GOPATH=$HOME/go
+          export PATH=$GOPATH/bin:$PATH
+          echo "🚀 Welcome to the Go & Protobuf Flake shell"
+        '';
+      };
+    };
+}
